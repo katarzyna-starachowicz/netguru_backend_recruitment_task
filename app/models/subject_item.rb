4a296f3e7e8bda75d66bd49acbcea7046026ebde
katarzyna-starachowicz <kasia@x8.io>
2016-10-21 17:35:41 +0200

    Add subject_item belongs_to teacher association

--- app/models/subject_item.rb
+++ app/models/subject_item.rb
@@ -1,7 +1,8 @@
 class SubjectItem < ActiveRecord::Base
-  has_many :participations, dependent: :destroy
-  has_many :students, through: :participations
-  has_many :subject_item_notes
+  has_many   :participations, dependent: :destroy
+  has_many   :students, through: :participations
+  has_many   :subject_item_notes
+  belongs_to :teacher
 
   validates :title, presence: true
 
