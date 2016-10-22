dc0f048289db1e8a9955162338abfc9798f7fecb
katarzyna-starachowicz <kasia@x8.io>
2016-10-22 23:09:08 +0200

    Set up visitors#index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  root 'visitors#index'
+
   resources :students do
     get :subjects
   end
 
   resources :teachers
-
-  resources :visitors, only: :index
 
   get 'reports/subjects', to: 'reports#subjects', as: 'report_subjects'
 end
